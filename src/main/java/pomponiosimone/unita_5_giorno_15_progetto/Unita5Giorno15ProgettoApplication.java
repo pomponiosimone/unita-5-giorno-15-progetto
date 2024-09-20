a0349e1c6fb4b3aae76f6cff9c87684383e04812
Simone Pomponio <Pomponio671@gmail.com>
2024-09-20 09:21:13 +0200

    Initial commit

---
+++ src/main/java/pomponiosimone/unita_5_giorno_15_progetto/Unita5Giorno15ProgettoApplication.java
@@ -0,0 +1,13 @@
+package pomponiosimone.unita_5_giorno_15_progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unita5Giorno15ProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unita5Giorno15ProgettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pomponiosimone/unita_5_giorno_15_progetto/Unita5Giorno15ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package pomponiosimone.unita_5_giorno_15_progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unita5Giorno15ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
